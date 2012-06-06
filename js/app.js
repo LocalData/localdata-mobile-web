648455319ff6e04b75214b7bab54df9c5159f586
hampelm <hampelm>
2012-06-06 10:11:19 -0700

    Switch to Detroit static loc for testing

--- js/app.js
+++ js/app.js
@@ -250,8 +250,8 @@ function drawMap() {
     map.locate({setView: true, maxZoom: 18});
     // var sf = new L.LatLng(37.77555050754543, -122.41365958293713);
     // For Detroit testing: 
-    //var detroit = new L.LatLng(42.305213, -83.126260);
-    //map.setView(detroit, 18);
+    var detroit = new L.LatLng(42.305213, -83.126260);
+    map.setView(detroit, 18);
     
 
     // Mark a location on the map. 
