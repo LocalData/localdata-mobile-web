6f5a1e16ed9c5f7c6a6c62ac9f61f6a049fc99e6
hampelm <hampelm>
2012-06-12 11:31:05 -0700

    fixes issue where parking doesn't show up after submitting an empty lot

--- js/settings.js
+++ js/settings.js
@@ -3,11 +3,13 @@ var settings = {
   
 };
 
-var BASEURL = 'http://surveyapidev.herokuapp.com/'; //'http://surveydet.herokuapp.com'; // no trailing slash
-//BASEURL = 'http://localhost:3000';
+var BASEURL = 'http://surveydet.herokuapp.com'; // no trailing slash
+// BASEURL = 'http://localhost:3000';
+// BASEURL = 'http://surveyapidev.herokuapp.com/';
+
 var GEOAPI = 'http://stormy-mountain-3909.herokuapp.com';
 var SURVEYID = '23206450-a0ac-11e1-ae6a-a17fba15c6fd'; // WSU
-SURVEYID = '69bef580-88eb-11e1-af82-a7908e0eb230'; //test survey
+//SURVEYID = '69bef580-88eb-11e1-af82-a7908e0eb230'; //test survey
 
 var locale = "detroit-transparent"; // our current set of parcels. 
 
