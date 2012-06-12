772e42d7cc809c5ee798c3971b394feb61ce80f5
hampelm <hampelm>
2012-06-11 17:36:40 -0700

    Use dev API server for test

--- js/settings.js
+++ js/settings.js
@@ -3,7 +3,7 @@ var settings = {
   
 };
 
-var BASEURL = 'http://surveydet.herokuapp.com'; // no trailing slash
+var BASEURL = 'http://surveyapidev.herokuapp.com/'; //'http://surveydet.herokuapp.com'; // no trailing slash
 //BASEURL = 'http://localhost:3000';
 var GEOAPI = 'http://stormy-mountain-3909.herokuapp.com';
 var SURVEYID = '23206450-a0ac-11e1-ae6a-a17fba15c6fd'; // WSU
