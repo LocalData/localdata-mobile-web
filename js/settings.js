87bc1e740f497754071bc310b3e9683c8ff89c57
hampelm <hampelm>
2012-06-11 17:37:08 -0700

    Use dev survey id

--- js/settings.js
+++ js/settings.js
@@ -7,7 +7,7 @@ var BASEURL = 'http://surveyapidev.herokuapp.com/'; //'http://surveydet.herokuap
 //BASEURL = 'http://localhost:3000';
 var GEOAPI = 'http://stormy-mountain-3909.herokuapp.com';
 var SURVEYID = '23206450-a0ac-11e1-ae6a-a17fba15c6fd'; // WSU
-//SURVEYID = '69bef580-88eb-11e1-af82-a7908e0eb230'; //test survey
+SURVEYID = '69bef580-88eb-11e1-af82-a7908e0eb230'; //test survey
 
 var locale = "detroit-transparent"; // our current set of parcels. 
 
