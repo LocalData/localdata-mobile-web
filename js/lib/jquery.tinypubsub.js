a547a3e8e727241dd1685dbf65dc43f21217c0b3
hampelm <hampelm>
2012-07-03 17:00:43 -0500

    No git, I just moved wax

---
+++ js/lib/jquery.tinypubsub.js
@@ -0,0 +1,22 @@
+/* jQuery Tiny Pub/Sub - v0.7 - 10/27/2011
+ * http://benalman.com/
+ * via https://gist.github.com/661855
+ * Copyright (c) 2011 "Cowboy" Ben Alman; Licensed MIT, GPL */
+
+(function($) {
+
+  var o = $({});
+
+  $.subscribe = function() {
+    o.on.apply(o, arguments);
+  };
+
+  $.unsubscribe = function() {
+    o.off.apply(o, arguments);
+  };
+
+  $.publish = function() {
+    o.trigger.apply(o, arguments);
+  };
+
+}(jQuery));
