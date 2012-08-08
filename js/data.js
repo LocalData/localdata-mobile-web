da26bf2ae5477138e459f3e21dbf0d0500303624
hampelm <hampelm>
2012-08-07 23:30:20 -0700

    Real survey questions

--- js/data.js
+++ js/data.js
@@ -1,73 +1,148 @@
 NSB.test = {
   "questions": [
-      { "name" : "use",
-        "text" : "What's the use?",
+      { "name" : "site",
+        "text" : "What's on the site?",
         "answers" : [
-            { "value" : "retail",
-              "text" : "Retail",
-              "questions" : [
-                { "name" : "retail-type",
-                  "text" : "What type of retail?",
-                  "answers" : [
-                    { "value" : "best",
-                      "text" : "the best type of retail",
-                      "questions" : [
-                        { "name": "good-level",
-                          "value" : "good-level",
-                          "text" : "How good?",
-                          "answers" : [
-                            { "value" : "3",
-                              "text" : "excellent"
-                            },
-                            { "value" : "2",
-                              "text" : "quite good"
-                            },
-                            { "value" : "1",
-                              "text" : "pretty good"
-                            }
-                          ]
-                        }
-                      ]
-                    },
-                    { "value" : "worst",
-                      "text" : "the worst type of retail"
-                    }
-                  ]
-                },
-                { "name" : "vod",
-                  "text" : "Vacant+Open+Dangerous?",
-                  "answers" : [
-                    { "value" : "1",
-                      "text" : "yes"
-                    },
-                    {
-                      "value" : "0",
-                      "text" : "no"
-                    }
-                  ]
-                }
-              ]
+            { 
+              "value" : "single-building",
+              "text" : "One building"
+              //"questions" : [
+              //  { "name" : "retail-type",
+              //    "text" : "What type of retail?",
+              //    "answers" : [
+              //      { "value" : "best",
+              //        "text" : "the best type of retail",
+              //        "questions" : [
+              //          { "name": "good-level",
+              //            "value" : "good-level",
+              //            "text" : "How good?",
+              //            "answers" : [
+              //              { "value" : "3",
+              //                "text" : "excellent"
+              //              },
+              //              { "value" : "2",
+              //                "text" : "quite good"
+              //              },
+              //              { "value" : "1",
+              //                "text" : "pretty good"
+              //              }
+              //            ]
+              //          }
+              //        ]
+              //      },
+              //      { "value" : "worst",
+              //        "text" : "the worst type of retail"
+              //      }
+              //    ]
+              //  }
+              //]
             },
-            { "value" : "service",
-              "text" : "Service",
-              "questions" : [
-                { "name" : "service-type",
-                  "text" : "What type of service?",
-                  "answers" : [
-                    { "value" : "dry-cleaner",
-                      "text" : "A Dry Cleaner, yo!"
-                    },
-                    { "value" : "other",
-                      "text" : "Other Service"
-                    }
-                  ]
-                }
-              ]
+            { 
+              "name" : "multiple-buildings",
+              "text" : "Multiple Buildings"
             },
-            { "value" : "storage",
-              "text" : "Storage"
+            { 
+              "name" : "parking",
+              "text" : "A parking lot"
+            },
+            { 
+              "name" : "A park",
+              "text" : "An empty lot"
             }
         ]
-      }
+      }, // End use
+      
+      { "name" : "use",
+        "text" : "What is the property used for",
+        "answers": [
+          {
+            "value": "retail",
+            "text": "Retail"
+          },
+          {
+            "value": "service",
+            "text": "Service"
+          },
+          {
+            "value": "restaurant",
+            "text": "Restaurant or Bar"
+          },
+          {
+            "value": "office",
+            "text": "Office"
+          },
+          {
+            "value": "industrial",
+            "text": "Industrial"
+          },
+          {
+            "value": "religious",
+            "text": "Religious"
+          },
+          {
+            "value": "unknown",
+            "text": "Unknown"
+          }
+        ]
+      },
+      
+      { "name": "occupancy",
+        "text": "Is it occupied?",
+        "answers": [
+          {
+            "name": "occupied",
+            "text": "Occupied"
+          },
+          {
+            "name": "probably-occupied",
+            "text": "Probably occupied"
+          },
+          {
+            "name": "probably-vacant",
+            "text": "Probably vacant"
+          },
+          {
+            "name": "vacant-abandoned",
+            "text": "Vacant, abandoned"
+          }
+        ]
+      },
+      
+      {
+        "name": "condition",
+        "text": "What condition is it in?",
+        "answers": [
+          {
+            "name": "good",
+            "text": "Good"
+          },
+          {
+            "name": "fair",
+            "text": "Fair"
+          },
+          {
+            "name": "poor",
+            "text": "Poor"
+          },
+          {
+            "name": "demolish",
+            "text": "Demolish"
+          }
+        ]
+      }, // end condition
+      
+      {
+        "name": "",
+        "text": "",
+        "answers": [
+          {
+            "name": "vod",
+            "text": "Vacant, open, and dangerous"
+          }
+        ]
+      } // end vod
+      
+      
+      
   ]
 };
