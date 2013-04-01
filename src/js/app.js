b5fa1878b018882cf8a38579cd7fda4dd04508a8
Prashant Singh <prashant@codeforamerica.org>
2013-04-01 15:51:44 -0700

    Reload app when we detect an update
    When the application loads, the app cache checks for updates. If we find an update, we load the updated app instead of proceeding with the old one.

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,7 @@ define(function (require) {
   var api = require('api');
   var FormView = require('form');
   var MapView = require('map');
+  var appCacheManager = require('app-cache-manager');
 
   // Listen for status-change events and adjust the UI accordingly.
   function setupEventHandlers() {
@@ -43,52 +44,60 @@ define(function (require) {
     init: function () {
       console.log("Initialize NSB");
 
-      // Get the survey, slug, etv.
-      var surveyPromise = api.getSurveyFromSlug();
+      appCacheManager.init(function () {
+        // Get the survey, slug, etv.
+        var surveyPromise = api.getSurveyFromSlug();
 
-      // Set the collector name, if we already know it.
-      if ($.cookie('collectorName') !== null){
-        $("#collector_name").val($.cookie('collectorName'));
-      }
+        var $collectorName = $('#collector_name');
+        var $collectorNameSubmit = $('#collector-name-submit');
+        $('#loading-msg').hide();
+        $collectorName.show();
+        $collectorNameSubmit.show();
 
-      $("#collector-name-submit").click(function(event) {
-        console.log("Setting collector name");
+        // Set the collector name, if we already know it.
+        if ($.cookie('collectorName') !== null){
+          $collectorName.val($.cookie('collectorName'));
+        }
 
-        app.collectorName = $("#collector_name").val();      
+        $collectorNameSubmit.click(function(event) {
+          console.log("Setting collector name");
 
-        $('#startpoint h2').html('Loading...');
+          app.collectorName = $collectorName.val();      
 
-        $('.collector').val(app.collectorName);
+          $('#startpoint h2').html('Loading...');
 
-        // Set a cookie with the collector's name
-        $.cookie('collectorName', app.collectorName, { path: '/' });
+          $('.collector').val(app.collectorName);
 
-        // Hide the homepage, show the survey
-        $('#home-container').slideToggle();
-        $('#survey-container').slideToggle();
-        $('body').attr('id', 'survey');
+          // Set a cookie with the collector's name
+          $.cookie('collectorName', app.collectorName, { path: '/' });
 
-        // Wait until we have the survey data
-        surveyPromise.done(function (survey) {
-          api.init(function (error) {
-            if (error) {
-              $('#startpoint h2').html('Sorry, something went wrong. Please reload the page.');
-              return;
-            }
+          // Hide the homepage, show the survey
+          $('#home-container').slideToggle();
+          $('#survey-container').slideToggle();
+          $('body').attr('id', 'survey');
 
-            setupEventHandlers();
+          // Wait until we have the survey data
+          surveyPromise.done(function (survey) {
+            api.init(function (error) {
+              if (error) {
+                $('#startpoint h2').html('Sorry, something went wrong. Please reload the page.');
+                return;
+              }
 
-            if (survey.type === 'point') {
-              $('#startpoint h2').html('Welcome, ' + app.collectorName + '<br>Pan and add a point to begin');
-            } else {
-              $('#startpoint h2').html('Welcome, ' + app.collectorName + '<br>Tap a parcel to begin');
-            }
+              setupEventHandlers();
 
-            app.map = new MapView(app, 'map-div');
-            app.f = new FormView(app, '#form');
+              if (survey.type === 'point') {
+                $('#startpoint h2').html('Welcome, ' + app.collectorName + '<br>Pan and add a point to begin');
+              } else {
+                $('#startpoint h2').html('Welcome, ' + app.collectorName + '<br>Tap a parcel to begin');
+              }
+
+              app.map = new MapView(app, 'map-div');
+              app.f = new FormView(app, '#form');
+            });
           });
-        });
-      }); 
+        }); 
+      });
     },
 
     // We'll use this to keep track of the object currently selected in the app
