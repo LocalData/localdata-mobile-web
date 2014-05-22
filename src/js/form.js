618d8fdf073ab9a6238a445202fc0b6401b84a18
Prashant Singh <prashant@codeforamerica.org>
2014-05-22 12:28:44 -0700

    Handle checkbox answers like other questions

--- src/js/form.js
+++ src/js/form.js
@@ -345,7 +345,11 @@ define(function (require) {
     function makeClickHandler($el) {
       return function handleClick(e) {
         var $this = $(this);
-        hideAndClearSubQuestionsFor($this);
+        if ($this.attr('type') === 'checkbox') {
+          hideAndClearSubQuestionsFor($this);
+        } else {
+          hideAndClearSubQuestionsFor($el);
+        }
 
         // Show the conditional questions for this response.
         if($this.prop("checked")) {
@@ -687,6 +691,15 @@ define(function (require) {
         }
 
         // Handle conditional questions.
+
+        // If this is a checkbox, use the IDs of the answers.
+        var checkboxes = $el.find('div.ui-checkbox > input');
+        _.each(checkboxes, function (input) {
+          var subq = app.questionsByParentId[$(input).attr('id')];
+          questionsToProcess = questionsToProcess.concat(subq);
+        });
+
+        // Use the question ID, which is appropriate for non-checkbox questions.
         var subQuestions = app.questionsByParentId[$el.attr('id')];
         if (subQuestions !== undefined) {
           questionsToProcess = questionsToProcess.concat(subQuestions);
