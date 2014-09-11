257475c7b75951e69d2c55699af98a3e1d03fd01
Matt Hampel <matth@localdata.com>
2014-09-11 17:13:58 -0400

    In some circumstances, bringToBack() doesn't seem to be available in this chain

--- src/js/map.js
+++ src/js/map.js
@@ -441,7 +441,8 @@ define(function (require) {
           circle = new L.Circle(e.latlng, radius, {
             clickable: false
           });
-          map.addLayer(circle).bringToBack();
+          map.addLayer(circle);
+          circle.bringToBack();
         }
         map.setView(e.latlng, 19);
 
